0b959f99378947ae51f247ea0dd9978d8276effb
zeyneptekin <zeyneptekin1682@gmail.com>
2024-09-20 23:02:16 +0300

    [add] click counter field

--- api/src/couch-base-adapter/couch-base-adapter.service.ts
+++ api/src/couch-base-adapter/couch-base-adapter.service.ts
@@ -10,6 +10,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class CouchbaseService {
+  private cluster: Cluster;
   private bucket: Bucket;
   private collection: Collection;
   private logger = new Logger(CouchbaseService.name);
@@ -20,12 +21,12 @@ export class CouchbaseService {
 
   async connect(): Promise<void> {
     try {
-      const cluster = await Cluster.connect('couchbase://localhost', {
+      this.cluster = await Cluster.connect('couchbase://localhost', {
         username: 'Administrator',
         password: '017985',
       });
 
-      this.bucket = cluster.bucket('doc1');
+      this.bucket = this.cluster.bucket('doc1');
       const scope = this.bucket.scope('_default');
       this.collection = scope.collection('_default');
 
@@ -43,15 +44,11 @@ export class CouchbaseService {
         await this.connect();
       }
 
-      let existingMapping;
-      try {
-        existingMapping = await this.collection.get(longUrl);
-        return existingMapping.content.shortUrl;
-      } catch (err) {
-        if (!(err instanceof DocumentNotFoundError)) {
-          this.logger.error('Error retrieving long URL mapping:', err);
-          throw err;
-        }
+      const query = `SELECT shortUrl FROM \`doc1\` WHERE longUrl = $1 LIMIT 1`;
+      const result = await this.cluster.query(query, { parameters: [longUrl] });
+
+      if (result.rows.length > 0) {
+        return result.rows[0].shortUrl;
       }
 
       let shortUrl: string;
@@ -69,7 +66,11 @@ export class CouchbaseService {
         }
       } while (true);
 
-      await this.collection.upsert(shortUrl, { longUrl });
+      await this.collection.upsert(shortUrl, {
+        clickCounter: 0,
+        longUrl,
+        shortUrl,
+      });
       this.logger.log(
         `URL mapping saved successfully with shortUrl: ${shortUrl}`,
       );
@@ -85,8 +86,9 @@ export class CouchbaseService {
     const buffer = Buffer.from(uuid.replace(/-/g, ''), 'hex');
     const bigIntId = bufferToBigInt(buffer);
     const base62Id = base62Encode(bigIntId);
+    const shortenedBase62Id = base62Id.slice(0, 7);
 
-    return `www.xxx.com/${base62Id.padStart(7, '0')}`;
+    return `www.xyz.com/${shortenedBase62Id}`;
   }
 
   async getLongUrl(shortUrl: string): Promise<string> {
@@ -95,23 +97,49 @@ export class CouchbaseService {
         throw new HttpException('Short URL parameter is required', 400);
       }
 
-      const result = await this.collection.get(shortUrl);
-      return result.content.longUrl;
-    } catch (error) {
-      if (error instanceof DocumentNotFoundError) {
+      const selectQuery = `
+      SELECT longUrl, clickCounter
+      FROM \`doc1\`
+      WHERE shortUrl = $1
+      LIMIT 1;
+    `;
+
+      const selectResult = await this.cluster.query(selectQuery, {
+        parameters: [shortUrl],
+      });
+
+      if (selectResult.rows.length === 0) {
         this.logger.error(`No mapping found for short URL: ${shortUrl}`);
         throw new NotFoundException(
           `No mapping found for short URL: ${shortUrl}`,
         );
-      } else {
-        this.logger.error(
-          `Error getting long URL for short URL ${shortUrl}: ${error.message}`,
-        );
-        throw new HttpException(
-          `Error retrieving long URL: ${error.message}`,
-          500,
-        );
       }
+
+      const { longUrl, clickCounter } = selectResult.rows[0];
+
+      const updateQuery = `
+      UPDATE \`doc1\`
+      SET clickCounter = $1
+      WHERE shortUrl = $2;
+    `;
+
+      await this.cluster.query(updateQuery, {
+        parameters: [clickCounter + 1, shortUrl],
+      });
+
+      this.logger.log(
+        `Updated count for short URL: ${shortUrl}, click counter: ${clickCounter + 1}`,
+      );
+
+      return longUrl;
+    } catch (error) {
+      this.logger.error(
+        `Error getting long URL for short URL ${shortUrl}: ${error.message}`,
+      );
+      throw new HttpException(
+        `Error retrieving long URL: ${error.message}`,
+        500,
+      );
     }
   }
 }
