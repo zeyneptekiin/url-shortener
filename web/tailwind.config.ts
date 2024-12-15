cc8d381f07194d3d7b5e691797f6db7806c2277a
zeyneptekin <zeyneptekin1682@gmail.com>
2024-12-15 16:49:16 +0300

    [add] custom colors to tailwind.config.ts

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        lightpurple: "#8B5DFF",
+        darkpurple: "#6A42C2",
       },
     },
   },
